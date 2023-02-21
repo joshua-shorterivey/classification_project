d155059a6ed01a328d95d7568e56831325bf112f
joshua-shorterivey <joshua.shorterivey@gmail.com>
2023-02-21 10:11:55 -0700

    amended prepare.py

--- prepare.py
+++ prepare.py
@@ -11,25 +11,27 @@ def prep_telco(df):
     ---
     returns: the prepared dataframe
     """
-    df = df.drop(columns=['customer_id','internet_service_type_id', 'payment_type_id', 'contract_type_id'])
+    df = df.drop(columns=['customer_id','internet_service_type_id',
+                          'payment_type_id', 'contract_type_id'])
     
     df['gender_encoded'] = df.gender.map({'Female':1, 'Male':0})
     df['partner_encoded'] = df.partner.map({'Yes':1, 'No':0})
     df['dependents_encoded'] = df.dependents.map({'Yes':1, 'No':0})
     df['phone_service_encoded'] = df.phone_service.map({'Yes':1, 'No':0})
-    df['paperless_billing_encoded'] = df.paperless_billing.map({'Yes':1, 'No':0})
+    df['paperless_billing_encoded'] = df.paperless_billing.map({'Yes':1, 
+                                                                'No':0})
     df['churn_encoded'] = df.churn.map({'Yes':1, 'No':0})
 
-    dummy_telco = pd.get_dummies(df[['contract_type',\
-                                    'payment_type', \
-                                    'internet_service_type', \
-                                    'multiple_lines', \
-                                    'online_security',\
-                                    'online_backup',\
-                                    'device_protection',\
-                                    'tech_support',\
-                                    'streaming_tv',\
-                                    'streaming_movies']],\
+    dummy_telco = pd.get_dummies(df[['contract_type',
+                                    'payment_type', 
+                                    'internet_service_type', 
+                                    'multiple_lines',
+                                    'online_security',
+                                    'online_backup',
+                                    'device_protection',
+                                    'tech_support',
+                                    'streaming_tv',
+                                    'streaming_movies']],
                                  dummy_na=False, drop_first=True)
     
     
@@ -51,8 +53,9 @@ def final_prep(df):
     df = df.select_dtypes(exclude='object')
 
     # rename binary encoded columns
-    df.rename(columns={'gender_encoded': 'gender', 'partner_encoded': 'partner', \
-                    'dependents_encoded': 'dependents', 'phone_service_encoded': 'phone_service', \
+    df.rename(columns={'gender_encoded': 'gender',
+                    'partner_encoded': 'partner', 
+                    'dependents_encoded': 'dependents', 'phone_service_encoded': 'phone_service', 
                     'paperless_billing_encoded': 'paperless_billing'})
 
     return df   
@@ -71,7 +74,6 @@ def split_data(df, target):
     
     #verify shapes of prepared df, train, validate, and test subsets
     print(f'Prepared df: {df.shape}')
-    print()
     print(f'Train: {train.shape}')
     print(f'Validate: {validate.shape}')
     print(f'Test: {test.shape}')
